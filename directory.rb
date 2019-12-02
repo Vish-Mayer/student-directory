636c23c82eea9c9a6f774f64b5f489941045b133
Vish Mayer <vish.mayer@gmail.com>
2019-12-02 14:03:32 +0000

    Adding more information

--- directory.rb
+++ directory.rb
@@ -1,25 +1,25 @@
 # array of students
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex Delarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffery Baratheon",
-"Norman Bates"
+["Dr. Hannibal Lecter", :november],
+["Darth Vader", :november],
+["Nurse Ratched", :november],
+["Michael Corleone", :november],
+["Alex Delarge", :november],
+["The Wicked Witch of the West", :november],
+["Terminator", :november],
+["Freddy Krueger", :november],
+["The Joker", :november],
+["Joffery Baratheon", :november],
+["Norman Bates", :november]
 ]
 
 def print_header
 puts "The students of Villains Academy"
 puts "-----------------"
 end
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |name|
+    puts "#{name[0]} (#{name[1]} cohort)"
   end
 end
 def print_footer(names)
