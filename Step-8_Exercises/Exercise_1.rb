776e5ae708374bdf3844f5b970a283fd6c1aa00f
Vish Mayer <vish.mayer@gmail.com>
2019-12-02 15:37:38 +0000

    Adding index

---
+++ Step-8_Exercises/Exercise_1.rb
@@ -0,0 +1,38 @@
+=begin
+1. We're using the each() method to iterate over an array of students. How can you
+modify the program to print a number before the name of each student,
+e.g. "1. Dr. Hannibal Lecter"? Hint: look into each_with_index()
+
+=end
+def input_students
+  puts "Please enter the names of the students"
+  puts "To fnish, just hit 'return' twice"
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students.push({name: name, cohort: :november})
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+
+def print_header
+puts "The students of Villains Academy"
+puts "-----------------"
+end
+def print(students)
+  students.each_with_index do |student, index| # 1. index number added to student.
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+def print_footer(names)
+puts "Overall, we have #{names.count} great students "
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
