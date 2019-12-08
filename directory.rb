bf20f9f02bb9eb24f0d633b7ea7f1501cf815117
Vish Mayer <vish.mayer@gmail.com>
2019-12-08 20:36:19 +0000

    Takes argument from command line

--- directory.rb
+++ directory.rb
@@ -10,20 +10,32 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
-    name, cohort = line.chomp.split(",")
+  name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
-end
-file.close
+  end
+  file.close
 end
 
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
+end
+
+
 def interactive_menu
-  @students = []
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -62,12 +74,12 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit 'return' twice"
   @students = []
-  name = gets.chomp
+  name = STDIN.gets.chomp
 
   while !name.empty? do
     @students.push({name: name, cohort: :november})
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
   @students
 end
@@ -85,4 +97,5 @@ def print_footer
 puts "Overall, we have #{@students.count} great students ".center(50,"-")
 end
 
+try_load_students
 interactive_menu
