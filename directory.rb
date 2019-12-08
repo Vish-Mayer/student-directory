7d2f11cc36cd9b2f520d2fd7c4cd8e18e9b50a50
Vish Mayer <vish.mayer@gmail.com>
2019-12-08 18:54:14 +0000

    Refactored menu

--- directory.rb
+++ directory.rb
@@ -1,59 +1,63 @@
+@students = []
 
 def interactive_menu
-  students = [] 
+  @students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
+    print_menu
+    process(gets.chomp)
+  end
+end
 
-    selection = gets.chomp
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
+  def show_students
+    print_header
+    print_student_list
+    print_footer
+  end
+
+  def process(selection)
     case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit
     else
       puts "I dont understand, try again"
     end
   end
-end
-
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit 'return' twice"
-  students = []
+  @students = []
   name = gets.chomp
 
   while !name.empty? do
-    students.push({name: name, cohort: :november})
-    puts "Now we have #{students.count} students"
+    @students.push({name: name, cohort: :november})
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  students
+  @students
 end
-
 
 def print_header
-puts "The students of Villains Academy"
-puts "-----------------"
+puts "The students of Villains Academy".center(50,"-")
+puts "-----------------".center(50)
 end
-def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+def print_student_list
+  @students.each_with_index do |student, index|
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
   end
 end
-def print_footer(names)
-puts "Overall, we have #{names.count} great students "
+def print_footer
+puts "Overall, we have #{@students.count} great students ".center(50,"-")
 end
 
 interactive_menu
-students = input_students
-print_header
-print(students)
-print_footer(students)
